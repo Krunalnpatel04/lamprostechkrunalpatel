a2c563ca40e7c5dd50409a5d034687a49522897c
Krunalnpatel04 <158278913+Krunalnpatel04@users.noreply.github.com>
2024-01-31 16:59:09 +0530

    Update mostfrequrntelement.java

--- mostfrequrntelement.java
+++ mostfrequrntelement.java
@@ -1,6 +1,24 @@
 public static mostfrequrntelement(){
   public static void Main(String []ags){
-      
+      int[] arr={5,2,5,3,5,3,1};
+      ArrayList<INTEGER> arr1=new ArrayList<>;
+      for(int i=o;i<arr.length;i++){
+          int count=1;
+        for(int k=0;k<arr1.length;k++){
+          if(arr[i]==arr1[k]){
+            continue;
+          }
+        }
+
+        for(int j=i+1;j<arr.length;j++){
+            
+          if(arr[i]==arr[j]){
+                count++;
+            }
+          if(count>1){
+            arr1.add(arr[i]);
+          }
+        }
   }
 }
 

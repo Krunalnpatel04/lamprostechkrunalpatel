747feb397da2705186e729c75c8c6d3d4a633962
Krunalnpatel04 <158278913+Krunalnpatel04@users.noreply.github.com>
2024-01-31 16:52:38 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+Public Static Main(){
+  public static void main(String args[]){
+      int[] arr={5,2,5,3,5,3,1};
+      ArrayList<INTEGER> arr1=new ArrayList<>;
+      for(int i=o;i<arr.length;i++){
+          int count=1;
+        for(int k=0;k<arr1.length;k++){
+          if(arr[i]==arr1[k]){
+            continue;
+          }
+        }
+
+        for(int j=i+1;j<arr.length;j++){
+            
+          if(arr[i]==arr[j]){
+                count++;
+            }
+          if(count>1){
+            arr1.add(arr[i]);
+          }
+        }
+  }
+}

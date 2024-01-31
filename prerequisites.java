3184b1749f453d6ae54556ee61d701e0f80e6c5e
Krunalnpatel04 <158278913+Krunalnpatel04@users.noreply.github.com>
2024-01-31 17:13:05 +0530

    Update prerequisites.java

--- prerequisites.java
+++ prerequisites.java
@@ -3,12 +3,18 @@ Public Static prerequisites.(){
       int []arr={10,2,4,5};
       String []arr1={"A","B","C","D"};
     int max=arr[0];
-    Sting s="";
+    char c;
+    String s="";
     for(int i=0;i<arr.length();i++){
       for(int j=i+1;j<arr.length();j++){
         if(arr[j]>max){
           max=arr[j];
+          c=j;
         }
       }
+      s=c +">";
+      
   }
+}
+  System.out.println(s)
 }
